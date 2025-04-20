ddf68fc587942dc63fc7439e4f23179bbbae75fa
jumbaumba <dmitrijborisuk21@gmail.com>
2025-04-20 18:30:23 +0300

    ф

--- TAW/client/src/app/App.tsx
+++ TAW/client/src/app/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Header } from '../widgets/ui/header/Header';
+import { Header } from '@widgets/ui/header/Header';
 import { Outlet } from 'react-router-dom';
 
 interface DataResponse {
